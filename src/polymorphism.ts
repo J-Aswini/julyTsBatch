d10fde033e516fad9bd748dd379d28ab173d1567
Aswini J <aswini.jan2002@gmail.com>
2026-08-23 16:49:04 +0530

    "polymorphism"

---
+++ src/polymorphism.ts
@@ -0,0 +1,14 @@
+class ParentClass1 {
+    amount = 12445
+    greet(){
+        console.log("Hello world")
+    }
+}
+ class ChildClass1 extends ParentClass{
+    study(){
+        console.log("learning")
+    }
+    greet(){
+        console.log("Hello world ts")
+    }
+ }
